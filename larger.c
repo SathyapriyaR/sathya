d5119e93ffd03011d19604ca8e815e68ae46f77c
SathyapriyaR <30828549+SathyapriyaR@users.noreply.github.com>
2018-01-18 16:37:35 +0530

    Create larger.c

---
+++ larger.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+printf("enter the values")
+scanf("%d,%d,%d",&a&b&c);
+if(a>b&&a>c)
+printf("a is larger");
+elseif(b>c&&b>a)
+printf("bis larger");
+else
+printf("c is larger");
+}
