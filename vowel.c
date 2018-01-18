e7ff4b08f70d0390b9c5003cb2c65dd0260a152d
SathyapriyaR <30828549+SathyapriyaR@users.noreply.github.com>
2018-01-18 16:12:32 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char ch;
+printf("enter the character");
+scanf("%c",&ch);
+if(ch='a'||ch='e'||ch='i'||ch='o'||ch='u')
+{
+printf("these are vowels")
+}
+else
+{
+printf("these ara consonent")
+}
