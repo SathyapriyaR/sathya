35abf0ceaf31e43a8ed4ba4d33de489c44295651
SathyapriyaR <30828549+SathyapriyaR@users.noreply.github.com>
2018-01-19 16:38:16 +0530

    Create eveninterval.c

---
+++ eveninterval.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int n,start num,end num,i;
+printf("Enter the number");
+scanf("%d",&n);
+for(i=start num;i<=end num;i++)
+{
+if(n%2==0)
+printf("%d is even num");
+else
+printf("%d is not a even number");
+}
+}
