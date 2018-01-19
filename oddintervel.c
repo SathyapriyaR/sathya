d74ef1d0491e58de5345d28299b047987e5abf80
SathyapriyaR <30828549+SathyapriyaR@users.noreply.github.com>
2018-01-19 16:42:10 +0530

    Create oddintervel.c

---
+++ oddintervel.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int n,start num,end num,i;
+printf("Enter the number");
+scanf("%d",&n);
+for(i=start num;i<=end num;i++)
+{
+if(n%2!=0)
+printf("%d is odd num");
+else
+printf("%d is not a odd numb");
+}
+}
