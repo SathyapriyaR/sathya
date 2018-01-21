b94038785cb3afc6c2f7fcaf2068f7b673806eab
SathyapriyaR <30828549+SathyapriyaR@users.noreply.github.com>
2018-01-21 11:58:03 +0530

    Create intmin.c

---
+++ intmin.c
@@ -0,0 +1,20 @@
+#include<stdio.h> 
+int main() 
+{
+   int a[30],i,n,max; 
+   printf("Enter no of elements");
+   scanf("%d", &n);  
+   printf("Enter the elements")
+   scanf("%d", &a[i]);
+   }
+   max=a[i]; 
+   for(i=0;i<n;i++) 
+   {
+      if(a[i]>max)
+      {
+         max= a[i];
+      }
+   } 
+   printf("max Element",max); 
+   return 0;
+  }
