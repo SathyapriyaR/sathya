fe03822fde859c5e41d2e60f19d232cdb0500a12
SathyapriyaR <30828549+SathyapriyaR@users.noreply.github.com>
2018-01-21 09:41:18 +0530

    Create multiple.c

---
+++ multiple.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,i,res=0;
+printf("Enter the number");
+scanf("%d",&n);
+for(i=1;i<=5;i++)
+{
+res=n*i;
+}
+printf("result is %d",res);
+}
