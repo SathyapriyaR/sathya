0e4d30d7fa57a2c0731b450b1b09f704957b7be8
SathyapriyaR <30828549+SathyapriyaR@users.noreply.github.com>
2018-01-23 16:40:00 +0530

    Create new file

---
+++ index.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int a[15],i,n;
+printf("Enter the size of array");
+scanf("%d",&a[i]);
+printf("Enter the elements");
+scanf("%d",&n);
+for(i=0;i<N;i++);
+{
+printf("%d%d",a[i],i);
+}
+}
