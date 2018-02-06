97045e8986115c154d040e2d69f3740e63d9bbb0
SathyapriyaR <30828549+SathyapriyaR@users.noreply.github.com>
2018-02-06 09:25:42 +0000

    Create 30.c

---
+++ 30.c
@@ -0,0 +1,13 @@
+#include"stdio.h"
+void main()
+{
+int h1,h2,m1,m2,res,tot;
+printf("\nEnter h1 and m1:");
+scanf("%d%d",&h1,&m1);
+printf("\nEnter h2 and m2:");
+scanf("%d%d",&h2,&m2);
+res=h1-h2;
+tot=m1-m2;
+printf("%d %d",res,tot);
+}
+
