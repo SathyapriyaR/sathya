8eb4bb79792a60d1147bf04d98e5950917360545
SathyapriyaR <30828549+SathyapriyaR@users.noreply.github.com>
2018-02-06 09:35:55 +0000

    Create 44.c

---
+++ 44.c
@@ -0,0 +1,17 @@
+#include"stdio.h"
+void main()
+{
+int i,n;
+printf("\nEnter the value of n");
+scanf("\n%d",&n);
+printf("\nEnter the value of i");
+scanf("\n%d",&i);
+if(i<=n)
+{
+printf("yes");
+}
+else
+{
+  printf("no");
+}
+}
