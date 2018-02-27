af3dc842115c3053767153f1103aad4c1ede3f4b
SathyapriyaR <30828549+SathyapriyaR@users.noreply.github.com>
2018-02-27 15:52:22 +0530

    Create 61.c

---
+++ 61.c
@@ -0,0 +1,16 @@
+#include"stdio.h"
+#include"string.h"
+void main()
+{
+char str[50],a[50];
+int i,k;
+printf("\nEnter the string:");
+scanf("%s",&str[i]);
+printf("\nEnter the number:");
+scanf("%d",&k);
+for(i=0;i<k;i++)
+{
+a[i]=str[i];
+}
+printf("\ncharacter is:%s",a);
+}
