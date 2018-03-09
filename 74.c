5c5fef7321e17924414dec0b48b20b8fa64ea56c
SathyapriyaR <30828549+SathyapriyaR@users.noreply.github.com>
2018-03-09 16:22:40 +0530

    Create 74.c

---
+++ 74.c
@@ -0,0 +1,11 @@
+#include"stdio.h"
+#include"math.h"
+void main()
+{
+float i;
+int res;
+printf("\nEnter the  number:");
+scanf("%f",&i);
+res=round(i);
+printf("\nThe round off value is %d",res);
+}
