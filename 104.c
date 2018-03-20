771f5732d954d74767bcf2195116bd349d991b60
SathyapriyaR <30828549+SathyapriyaR@users.noreply.github.com>
2018-03-20 16:08:11 +0530

    Create 104.c

---
+++ 104.c
@@ -0,0 +1,12 @@
+#include"stdio.h"
+#include"math.h"
+void main()
+{
+  int n,k,res;
+  printf("\nEnter the value of n:");
+  scanf("%d",&n);
+  printf("\nEnter the value of k:");
+  scanf("%d",&k);
+  res=pow(n,k);
+  printf("\n%d",res);
+}
